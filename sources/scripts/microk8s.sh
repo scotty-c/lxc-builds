d570983170da429c5b5951265c14132b1f752b9c
scott coulton <scott.coulton@microsoft.com>
2021-11-03 13:05:30 +1100

    fixing k8s

---
+++ sources/scripts/microk8s.sh
@@ -0,0 +1,11 @@
+ #!/bin/sh
+    set -eux
+    
+    echo "# microk8s..."
+    sudo snap install microk8s --classic --channel=1.22
+    mkdir -p /home/scott/.kube/
+    sudo usermod -a -G microk8s scott
+    sudo chown -f -R scott ~/.kube
+    sudo microk8s config > /home/scott/.kube/config
+    chmod 600 /home/scott/.kube/config
+    sudo microk8s.enable dns storage
