1fa9f7c36919a5e3e08afbc2a0da3fda058f296a
Scott Coulton <scotty-c@users.noreply.github.com>
2024-03-17 20:55:44 +1100

    Update kubespin.sh

--- sources/scripts/kubespin.sh
+++ sources/scripts/kubespin.sh
@@ -1,20 +1,22 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-
-# Install k3d
-k3d cluster create wasm-cluster \
-  --image ghcr.io/spinkube/containerd-shim-spin/k3d:v0.13.0 \
-  --port "8081:80@loadbalancer" \
-  --agents 2
-
-# Install cert-manager
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.3/cert-manager.yaml
-
-# Install runtimeclass
-kubectl apply -f https://github.com/spinkube/spin-operator/releases/download/v0.1.0/spin-operator.runtime-class.yaml
-
-# Install CRD's
+# Add crd's,runtime and shim
 kubectl apply -f https://github.com/spinkube/spin-operator/releases/download/v0.1.0/spin-operator.crds.yaml
+kubectl apply -f https://github.com/spinkube/spin-operator/releases/download/v0.1.0/spin-operator.runtime-class.yaml
+kubectl apply -f https://github.com/spinkube/spin-operator/releases/download/v0.1.0/spin-operator.shim-executor.yaml
+
+# Add Helm repository if not already done
+helm repo add kwasm http://kwasm.sh/kwasm-operator/
+
+# Install KWasm operator
+helm install \
+  kwasm-operator kwasm/kwasm-operator \
+  --namespace kwasm \
+  --create-namespace \
+  --set kwasmOperator.installerImage=ghcr.io/spinkube/containerd-shim-spin/node-installer:v0.13.0
+
+# Provision Nodes
+kubectl annotate node --all kwasm.sh/kwasm-node=true
 
 # Install Spin Operator with Helm
 helm install spin-operator \
@@ -23,49 +25,3 @@ helm install spin-operator \
   --version 0.1.0 \
   --wait \
   oci://ghcr.io/spinkube/charts/spin-operator
-
-# Install Shim Executor
-kubectl apply -f https://github.com/spinkube/spin-operator/releases/download/v0.1.0/spin-operator.shim-executor.yaml
-
-# Install metalLB
-for cluster_name in $(docker network list --format "{{ .Name}}" | grep k3d); do
-
-cidr_block=$(docker network inspect $cluster_name | jq '.[0].IPAM.Config[0].Subnet' | tr -d '"')
-cidr_base_addr=${cidr_block%???}
-ingress_first_addr=$(echo $cidr_base_addr | awk -F'.' '{print $1,$2,255,0}' OFS='.')
-ingress_last_addr=$(echo $cidr_base_addr | awk -F'.' '{print $1,$2,255,255}' OFS='.')
-ingress_range=$ingress_first_addr-$ingress_last_addr
-
-# switch context to current cluster
-kubectl config use-context $cluster_name
-
-# deploy metallb
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.3/config/manifests/metallb-native.yaml
-
-# configure metallb ingress address range
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  namespace: metallb-system
-  name: config
-data:
-  config: |
-    address-pools:
-    - name: default
-      protocol: layer2
-      addresses:
-      - $ingress_range
-EOF
-done
-
-# Install traefik
-helm repo add traefik https://helm.traefik.io/traefik
-helm repo update
-helm install traefik traefik/traefik --namespace traefik --create-namespace --values - <<EOF
-additionalArguments:
-  - "--providers.kubernetescrd"
-  - "--entrypoints.web.address=:80"
-  - "--entrypoints.websecure.address=:443"
-  - "--
-EOF
