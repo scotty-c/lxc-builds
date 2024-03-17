a61494847527af687e98d348b4097c707a796811
Scott Coulton <scotty-c@users.noreply.github.com>
2024-03-17 20:40:07 +1100

    Update microk8s.sh

--- sources/scripts/microk8s.sh
+++ sources/scripts/microk8s.sh
@@ -1,6 +1,6 @@
  #!/bin/sh
     set -eux
-    
+
     echo "# microk8s..."
     sudo snap install microk8s --classic --channel=1.28
     mkdir -p /home/scott/.kube/
@@ -8,4 +8,8 @@
     sudo chown -f -R scott ~/.kube
     sudo microk8s config > /home/scott/.kube/config
     chmod 600 /home/scott/.kube/config
-    sudo microk8s.enable dns storage
+    sudo microk8s.enable dns
+    sudo microk8s.enable community
+    sudo microk8s.enable cert-manager
+    sudo microk8s.enable metallb:10.134.81.204-10.134.81.208
+    sudo microk8s.enable traefik
